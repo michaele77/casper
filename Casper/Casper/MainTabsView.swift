c363b355d0095517d976d988453abf1e15f6fd36
Michael Ershov <mershov@Michaels-iMac.local>
2023-12-16 16:54:13 -0800

    Changed some symbols and added a note with regards to timer functionality. TODO: Continue work on the persistence manager

--- Casper/Casper/MainTabsView.swift
+++ Casper/Casper/MainTabsView.swift
@@ -14,22 +14,23 @@ struct MainTabsView: View {
     @State private var default_tab = 2
     @StateObject var asset_library_helper = AssetLibraryHelper()
 
-    // Create a timer that fires every 10 seconds.
+    // Create a timer that fires every 10 seconds. It will actually fire in the background even if the app is not open, so as long as the app is running, this seems like it will still work.
+    // TODO: Obviously, if the app gets shut down or crashes, the timer will no longer fire. This is ok for now, but really we eventually want to have a more consistent way of generating background tasks even if the app gets closed.
     let timer = Timer.publish(every: 10, on: .main, in: .common).autoconnect()
     
     var body: some View {
         TabView(selection:$default_tab) {
             FriendsView()
                 .tabItem {
-                    Label("Friends", systemImage: "list.dash")
+                    Label("Friends", systemImage: "person.3.sequence.fill")
                 }.tag(1)
             SessionView(globalVars: globalVars, assetLibraryHelper: asset_library_helper)
                 .tabItem {
-                    Label("Sessions", systemImage: "list.dash")
+                    Label("Sessions", systemImage: "network")
                 }.tag(2)
             SettingsView()
                 .tabItem {
-                    Label("Settings", systemImage: "list.dash")
+                    Label("Settings", systemImage: "gear")
                 }.tag(3)
         }
         .navigationBarBackButtonHidden(true)
